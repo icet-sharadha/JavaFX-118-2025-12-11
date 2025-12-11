a61bf19a9c75edbf5f81599f19d12e3bca1c7615
sharada-marasinha <sharadamarasinha@gmail.com>
2025-12-11 21:07:11 +0530

    final

--- src/controller/SampleController.java
+++ src/controller/SampleController.java
@@ -1,9 +1,16 @@
 package controller;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
 import model.Customer;
+
+import java.util.ArrayList;
 
 public class SampleController {
 
@@ -12,6 +19,14 @@ public class SampleController {
     public TextField txtAddress;
     public TextField txtSalary;
     public TextField txtId;
+    public TableView tblCustomerList;
+    public TableColumn colId;
+    public TableColumn colUserNAme;
+    public TableColumn colAddress;
+    public TableColumn colSalary;
+    public TableColumn colEmail;
+
+    ArrayList<Customer> customerArrayList = new ArrayList<>();
 
     public void btnAddCustomerOnAction(ActionEvent actionEvent) {
 
@@ -23,5 +38,23 @@ public class SampleController {
 
         Customer customer = new Customer(email, userName, address, salary, id);
         System.out.println(customer);
+        customerArrayList.add(customer);
+
+        System.out.println(customerArrayList);
     }
+
+    public void btnReloadOnAction(ActionEvent actionEvent) {
+        loadTable();
+    }
+    private void loadTable(){
+        colId.setCellValueFactory(new PropertyValueFactory<>("id"));
+        colUserNAme.setCellValueFactory(new PropertyValueFactory<>("name"));
+        colEmail.setCellValueFactory(new PropertyValueFactory<>("email"));
+        colAddress.setCellValueFactory(new PropertyValueFactory<>("address"));
+        colSalary.setCellValueFactory(new PropertyValueFactory<>("salary"));
+
+        tblCustomerList.setItems(FXCollections.observableList(customerArrayList));
+    }
+
+
 }
